697108d1eddcbab4beeede3149345ad0ea7c57aa
ssangamr <ssangamr@kent.edu>
2020-10-12 15:53:07 -0400

    Updated for PA, created production folder

--- bottle_1/bottle_app.py
+++ bottle_1/bottle_app.py
@@ -4,9 +4,10 @@ from bottle import get, post, template, request, redirect
 
 ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ
 
-assert ON_PYTHONANYWHERE == False
+
+#assert ON_PYTHONANYWHERE == False
 if ON_PYTHONANYWHERE:
-    pass
+    from bottle import default_app
 else:
     from bottle import  run, debug
 
@@ -18,7 +19,7 @@ def get_show_list():
     cursor = connection.cursor()
     cursor.execute("select * from todo")
     result = cursor.fetchall()
-    cursor.close() 
+    cursor.close()
     return template("show_list", rows=result)
 
 @get("/new_item")
@@ -26,19 +27,20 @@ def get_new_item():
     return template("new_item")
 
 @post("/new_item")
-def post_new_item(): 
+def post_new_item():
     new_item = request.forms.get("new_item").strip()
     connection = sqlite3.connect("todo.db")
     cursor = connection.cursor()
     cursor.execute("insert into todo (task, status) values(?,?)", (new_item,1))
     #cursor.lastrowid
     connection.commit()
-    cursor.close() 
+    cursor.close()
     #return "The new item is [" + new_item + "]..."
     redirect("/")
 
 if ON_PYTHONANYWHERE:
-    pass
+    application = default_app()
 else:
     debug(True)
     run(host='localhost', port=8080)
+
