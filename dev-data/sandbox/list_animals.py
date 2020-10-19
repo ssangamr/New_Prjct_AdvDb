131922974e64899feb8c6a8b38852f0c78b3c5a9
ssangamr <ssangamr@kent.edu>
2020-10-19 01:02:30 +0000

    deleting dev data

--- dev-data/sandbox/list_animals.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("animals.db")
-cursor = connection.cursor()
-cursor.execute("select * from pets where kind='Rabbit'")
-result = cursor.fetchall()
-print(result)
