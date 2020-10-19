52e9fd6b1260f89a9e81c384b69b988487cb4d2b
ssangamr <ssangamr@kent.edu>
2020-10-19 00:56:36 +0000

    Removed dev-data

---
+++ dev-data/bottle_app.py
@@ -0,0 +1,46 @@
+import os
+import sqlite3
+from bottle import get, post, template, request, redirect
+
+ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ
+
+
+#assert ON_PYTHONANYWHERE == True
+if ON_PYTHONANYWHERE:
+    from bottle import default_app
+else:
+    from bottle import  run, debug
+
+
+
+@get('/')
+def get_show_list():
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("select * from todo")
+    result = cursor.fetchall()
+    cursor.close()
+    return template("show_list", rows=result)
+
+@get("/new_item")
+def get_new_item():
+    return template("new_item")
+
+@post("/new_item")
+def post_new_item():
+    new_item = request.forms.get("new_item").strip()
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("insert into todo (task, status) values(?,?)", (new_item,1))
+    #cursor.lastrowid
+    connection.commit()
+    cursor.close()
+    #return "The new item is [" + new_item + "]..."
+    redirect("/")
+
+if ON_PYTHONANYWHERE:
+    application = default_app()
+else:
+    debug(True)
+    run(host='localhost', port=8080)
+
