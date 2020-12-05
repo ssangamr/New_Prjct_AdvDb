87c6e2f86947db45e1aaa1b0702c224265c0c8d2
ssangamr <ssangamr@kent.edu>
2020-12-05 06:40:09 +0000

    added storage.py abstraction module

--- bottle_app.py
+++ bottle_app.py
@@ -10,25 +10,34 @@ if ON_PYTHONANYWHERE:
     from bottle import default_app
 else:
     from bottle import  run, debug
+from storage import get_items, get_item, update_status, create_item, update_item, delete_item
+
+def update_item(id, updated_task):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("update todo set task=? where id=?", (updated_task,id))
+    connection.commit()
+    cursor.close()
+
+def delete_item(id):
+    connection = sqlite3.connect("todo.db")
+    cursor = connection.cursor()
+    cursor.execute("delete from todo where id=?", (id,))
+    connection.commit()
+    cursor.close()
+
+
 
 
 
 @get('/')
 def get_show_list():
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("select * from todo")
-    result = cursor.fetchall()
-    cursor.close()
+    result = get_items()
     return template("show_list", rows=result)
 
 @get("/set_status/<id:int>/<value:int>")
-def get_status(id,value):
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("Update todo set status=? where id=?", (value,id))
-    connection.commit()
-    cursor.close()
+def get_set_status(id,value):
+    update_status(id,value)
     redirect("/")
 
 @get("/new_item")
@@ -38,44 +47,27 @@ def get_new_item():
 @post("/new_item")
 def post_new_item():
     new_item = request.forms.get("new_item").strip()
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("insert into todo (task, status) values(?,?)", (new_item,1))
-    #cursor.lastrowid
-    connection.commit()
-    cursor.close()
-    #return "The new item is [" + new_item + "]..."
+    create_item(new_item, 1)
     redirect("/")
 
-@get("/delete_item/<id:int>")
-def get_delete_item(id):
-    print("we want to delete ", id)
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("delete from todo where id=?", (id,))
-    connection.commit()
-    cursor.close()
-    redirect("/")
 
 @get("/update_item/<id:int>")
 def get_update_item(id):
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("select * from todo where id=?",(id,))
-    result = cursor.fetchall()
-    cursor.close()
-    return template("update_item", row=result[0])
+    result = get_item(id)
+    return template("update_item", row=result)
 
 @post("/update_item")
 def post_update_item():
     id = int(request.forms.get("id").strip())
     updated_item = request.forms.get("updated_item").strip()
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("update todo set task=? where id=?", (updated_item,id))
-    connection.commit()
-    cursor.close()
+    update_item(id, updated_item)
     redirect("/")
+
+@get("/delete_item/<id:int>")
+def get_delete_item(id):
+    delete_item(id)
+    redirect("/")
+
 
 if ON_PYTHONANYWHERE:
     application = default_app()
