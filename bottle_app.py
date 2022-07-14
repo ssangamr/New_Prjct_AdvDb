b77beced0038b08e82e6965b0117b5b6ab519362
IT <it@Anveshs-MacBook-Air.local>
2022-07-13 23:51:01 -0400

    third commit on master

--- bottle_app.py
+++ bottle_app.py
@@ -33,6 +33,12 @@ def post_new_item():
     create_item(new_item, 1)
     redirect("/")
 
+@post("/new_item1")
+def post_new_item():
+    new_item = request.forms.get("new_item").strip()
+    create_item(new_item, 1)
+    redirect("/")
+
 
 @get("/update_item/<id:int>")
 def get_update_item(id):
